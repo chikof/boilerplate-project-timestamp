25a169a3f31b6ac30cd5e11ff6a9e26acf544da1
Chikof <chiko@envs.net>
2024-08-13 14:53:37 +0100

    chore: shorturl

--- index.js
+++ index.js
@@ -4,15 +4,18 @@
 // init project
 var express = require('express');
 var app = express();
-const { isValidDate, parseDate } = require('./validators');
+const { isValidDate, parseDate, isValidUrl } = require('./validators');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const bodyParser = require('body-parser');
 app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(req, res) {
@@ -25,7 +28,7 @@ app.get("/api/hello", function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-function validDateMiddleware(req, res, done) {
+function dateMiddleware(req, res, done) {
   const { date } = req.params;
 
   if (!date) {
@@ -44,6 +47,40 @@ function validDateMiddleware(req, res, done) {
   return res.json({ error: "Invalid date" });
 }
 
+const urls = new Map();
+
+function urlMiddleware(req, res, done) {
+  const { url } = req.body;
+
+  if (isValidUrl(url)) {
+    return done();
+  }
+
+  return res.json({ error: "invalid url" });
+}
+
+app.post("/api/shorturl", urlMiddleware, (req, res) => {
+  const { url } = req.body;
+  const response = {
+    original_url: url,
+    short_url: urls.size + 1
+  };
+
+  urls.set(response.short_url, response);
+
+  return res.json(response);
+});
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const { short_url } = req.params;
+  const url = urls.get(Number(short_url));
+  if (!url) {
+    return res.json({ error: "Invalid short URL" });
+  }
+
+  return res.redirect(url.original_url);
+});
+
 app.get("/api/whoami", (req, res) => {
   const { headers } = req;
   return res.json({
@@ -53,7 +90,7 @@ app.get("/api/whoami", (req, res) => {
   });
 });
 
-app.get("/api/:date?", validDateMiddleware, (req, res) => {
+app.get("/api/:date?", dateMiddleware, (req, res) => {
   const dateObject = parseDate(req.params.date);
 
   return res.json({
--- validators.js
+++ validators.js
@@ -5,10 +5,19 @@ function isValidDate(dateStr) {
   return !isNaN(date.getTime());
 }
 
+function isValidUrl(url) {
+  try {
+    const urlObject = new URL(url);
+    return ["http:", "https:"].includes(urlObject.protocol);
+  } catch {
+    return false;
+  }
+}
+
 function parseDate(dateStr) {
   const timestamp = Number(dateStr);
   const str = !isNaN(timestamp) ? timestamp : dateStr;
   return new Date(str);
 }
 
-module.exports = { isValidDate, parseDate }
+module.exports = { isValidDate, parseDate, isValidUrl }
